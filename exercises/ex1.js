5872c55a105d4f1983f92fb65d0a17ec6add8b4a
Andy Young <ayscript@gmail.com>
2019-07-15 15:38:28 -0600

    complete 'Intro'

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,1 +1,15 @@
-// My first Node.js program
+#!/usr/bin/env node
+
+"use strict";
+
+// process.stdin.read();
+printHelp();
+
+// *****************************8
+
+function printHelp() {
+  console.log("ex1 usage");
+  console.log(" ex1.js --help");
+  console.log("--help                print this help");
+  console.log("");
+}
